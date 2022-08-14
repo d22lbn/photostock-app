702655ffd1a7141fe6e0a30e36ae4196b23c47a2
ma4ikxin <d22lbn@gmail.com>
2022-08-14 12:52:55 +0300

    edit

--- src/redux/state.js
+++ src/redux/state.js
@@ -26,27 +26,19 @@ let getPhotos = (photoNumber) => {
         let rand = Math.round(Math.random() * 3);
         if (rand === 0) {
             photo = {
-                id: i,
-                name: "коала",
-                img: koalaPhoto,
+                id: i, name: "коала", img: koalaPhoto,
             }
         } else if (rand === 1) {
             photo = {
-                id: i,
-                name: "машина",
-                img: carPhoto,
+                id: i, name: "машина", img: carPhoto,
             }
         } else if (rand === 2) {
             photo = {
-                id: i,
-                name: "море",
-                img: seaPhoto,
+                id: i, name: "море", img: seaPhoto,
             }
         } else {
             photo = {
-                id: i,
-                name: "дерево",
-                img: treePhoto,
+                id: i, name: "дерево", img: treePhoto,
             }
         }
         photos.push(photo);
@@ -57,123 +49,79 @@ let getPhotos = (photoNumber) => {
 let store = {
     _state: {
         MainPage: {
-            title: "Размещайте и приобретайте красивые фотографии в несколько кликов",
-            iconsMobile: [
-                {
-                    id: 0,
-                    img: space,
-                },
-                {
-                    id: 1,
-                    img: space,
-                },
-                {
-                    id: 2,
-                    img: space,
-                },
-                {
-                    id: 3,
-                    img: space,
-                },
-                {
-                    id: 4,
-                    img: space,
-                },
-            ],
-            icons: [
-                {
-                    id: 0,
-                    img: dog,
-                },
-                {
-                    id: 1,
-                    img: tree,
-                },
-                {
-                    id: 2,
-                    img: car,
-                },
-                {
-                    id: 3,
-                    img: tower,
-                },
-                {
-                    id: 4,
-                    img: car,
-                },
-                {
-                    id: 5,
-                    img: dog,
-                },
-                {
-                    id: 6,
-                    img: tower,
-                },
-                {
-                    id: 7,
-                    img: tree,
-                },
-                {
-                    id: 8,
-                    img: tower,
-                },
-            ]
+            title: "Размещайте и приобретайте красивые фотографии в несколько кликов", iconsMobile: [{
+                id: 0, img: space,
+            }, {
+                id: 1, img: space,
+            }, {
+                id: 2, img: space,
+            }, {
+                id: 3, img: space,
+            }, {
+                id: 4, img: space,
+            },], icons: [{
+                id: 0, img: dog,
+            }, {
+                id: 1, img: tree,
+            }, {
+                id: 2, img: car,
+            }, {
+                id: 3, img: tower,
+            }, {
+                id: 4, img: car,
+            }, {
+                id: 5, img: dog,
+            }, {
+                id: 6, img: tower,
+            }, {
+                id: 7, img: tree,
+            }, {
+                id: 8, img: tower,
+            },]
         },
 
         PopularPage: {
-            popularCategories: [
-                {
-                    title: "Топ 7 новинок",
-                    photos: getPhotos(12),
-                },
-                {
-                    title: "Топ 7 наиболее скачиваемых",
-                    photos: getPhotos(7),
-                },
-            ]
+            popularCategories: [{
+                title: "Топ 7 новинок", photos: getPhotos(12),
+            }, {
+                title: "Топ 7 наиболее скачиваемых", photos: getPhotos(7),
+            },]
         },
 
         SearchPage: {
-            icon: loupe,
-            photos: getPhotos(22)
+            icon: loupe, photos: getPhotos(22)
         },
 
         RatingPage: {
-            title: "Лидеры продаж",
-            peoples: [
-                {
-                    id: 0,
-                    name: "Глеб Петров",
-                    text: "Увлекаюсь фотосъемкой с 12 лет. Побывал в 20 странах, из которых привез свыше 50000 новых интересных кадров!",
-                    ava: ava,
-                    photos: getPhotos(3),
-                    rating: 1
-                },
-                {
-                    id: 1,
-                    name: "Артем Иванов",
-                    text: "В 15 лет мне подарили фотоаппарат и понеслось... Любимая категория фотографий - портрет.",
-                    ava: ava,
-                    photos: getPhotos(3),
-                    rating: 2
-                },
-                {
-                    id: 2,
-                    name: "Артем Иванов",
-                    text: "В 15 лет мне подарили фотоаппарат и понеслось... Любимая категория фотографий - портрет.",
-                    ava: ava,
-                    photos: getPhotos(3),
-                    rating: 3
-                },
-                {
-                    id: 3,
-                    name: "Артем Иванов",
-                    text: "В 15 лет мне подарили фотоаппарат и понеслось... Любимая категория фотографий - портрет.",
-                    ava: ava,
-                    photos: getPhotos(3),
-                    rating: 4
-                },
-            ]
+            title: "Лидеры продаж", peoples: [{
+                id: 0,
+                name: "Глеб Петров",
+                text: "Увлекаюсь фотосъемкой с 12 лет. Побывал в 20 странах, из которых привез свыше 50000 новых интересных кадров!",
+                ava: ava,
+                photos: getPhotos(3),
+                rating: 1
+            }, {
+                id: 1,
+                name: "Артем Иванов",
+                text: "В 15 лет мне подарили фотоаппарат и понеслось... Любимая категория фотографий - портрет.",
+                ava: ava,
+                photos: getPhotos(3),
+                rating: 2
+            }, {
+                id: 2,
+                name: "Артем Иванов",
+                text: "В 15 лет мне подарили фотоаппарат и понеслось... Любимая категория фотографий - портрет.",
+                ava: ava,
+                photos: getPhotos(3),
+                rating: 3
+            }, {
+                id: 3,
+                name: "Артем Иванов",
+                text: "В 15 лет мне подарили фотоаппарат и понеслось... Любимая категория фотографий - портрет.",
+                ava: ava,
+                photos: getPhotos(3),
+                rating: 4
+            },]
         },
 
         LkPage: {
@@ -201,8 +149,7 @@ let store = {
             owner: "Борис",
             price: 12000
         }
-    },
-    _callSubscriber() {
+    }, _callSubscriber() {
     },
 
     getState() {
@@ -213,35 +160,42 @@ let store = {
     },
 
     dispatch(action) {
-        if (action.type === 'CHANGED-NAME') {
-            let newName = this._state.LkPage.newName;
-            if (newName.length > 0) {
-                this._state.LkPage.name = newName;
-                this._state.LkPage.newName = '';
-            }
-            this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NAME') {
-            this._state.LkPage.newName = action.newName;
-            this._callSubscriber(this._state);
-        } else if (action.type === 'CHANGED-SURNAME') {
-            let newSurname = this._state.LkPage.newSurname;
-            if (newSurname.length > 0) {
-                this._state.LkPage.surname = newSurname;
-                this._state.LkPage.newSurname = '';
-            }
-            this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-SURNAME') {
-            this._state.LkPage.newSurname = action.newSurname;
-            this._callSubscriber(this._state);
-        } else if (action.type === 'PUT-LIKE') {
-            if (this._state.PhotoPage.likeIconActive === like) {
-                this._state.PhotoPage.likeIconActive = likeActive;
-                this._state.PhotoPage.likes++;
-            } else {
-                this._state.PhotoPage.likeIconActive = like;
-                this._state.PhotoPage.likes--;
-            }
-            this._callSubscriber(this._state);
+        switch (action.type) {
+            case 'CHANGED-NAME':
+                let newName = this._state.LkPage.newName;
+                if (newName.length > 0) {
+                    this._state.LkPage.name = newName;
+                    this._state.LkPage.newName = '';
+                }
+                this._callSubscriber(this._state);
+                break;
+            case 'UPDATE-NAME':
+                this._state.LkPage.newName = action.newName;
+                this._callSubscriber(this._state);
+                break;
+            case 'CHANGED-SURNAME':
+                let newSurname = this._state.LkPage.newSurname;
+                if (newSurname.length > 0) {
+                    this._state.LkPage.surname = newSurname;
+                    this._state.LkPage.newSurname = '';
+                }
+                this._callSubscriber(this._state);
+                break;
+            case 'UPDATE-SURNAME':
+                this._state.LkPage.newSurname = action.newSurname;
+                this._callSubscriber(this._state);
+                break;
+            case 'PUT-LIKE':
+                if (this._state.PhotoPage.likeIconActive === like) {
+                    this._state.PhotoPage.likeIconActive = likeActive;
+                    this._state.PhotoPage.likes++;
+                } else {
+                    this._state.PhotoPage.likeIconActive = like;
+                    this._state.PhotoPage.likes--;
+                }
+                this._callSubscriber(this._state);
+                break;
+            default:
         }
     }
 }
